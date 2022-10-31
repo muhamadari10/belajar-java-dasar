967dc4a491b4b9c48929b3783c9502be58b3d96c
Ari Maulana <m.ari.maulana93@gmail.com>
2022-10-31 13:55:50 +0700

    [Belajara java dasar] - Tipe Data Char

---
+++ src/TipeDataChar.java
@@ -0,0 +1,11 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char a = 'A';
+        char r = 'R';
+        char i = 'I';
+
+        System.out.println(a);
+        System.out.println(r);
+        System.out.println(i);
+    }
+}
